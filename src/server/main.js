8705c2ce163d41b975e0a74f6b9303686e95d27c
djrcoder <djrcode@gmail.com>
2020-02-11 16:23:26 +0900

    Heroku

--- src/server/main.js
+++ src/server/main.js
@@ -29,13 +29,26 @@ app.get("/api/runlog", async (req, res) => {
     }
 });
 
+app.post("/api/runlog/run", async (req, res) => {
+    try {
+        const allLogs = await db.select().table("logs");
+        console.log("logs")
+        res.json(allLogs);
+    } catch (err) {
+        console.error("Error loading log!", err);
+        res.sendStatus(500);
+    }
+})
+
 
 
 // Always return the main index.html, so react - router render the route i
 // n the client
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../..", "build", "index.html"));
 });
+
+
 
 module.exports = app;
 
