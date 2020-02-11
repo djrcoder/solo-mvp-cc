7e7cb6456dec86d5dede9b1cc30f188d9c298f02
djrcoder <djrcode@gmail.com>
2020-02-11 21:41:24 +0900

    Add chart.js, Logo, CSS.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import InputForm from "./components/InputForm"
+import InputForm from "./components/InputForm";
+
+
+
+
+const tableData = [];
+
 
 // Render chart here 
 // when button is clicked, chart will re-draw with the latest data
@@ -8,17 +14,42 @@ import InputForm from "./components/InputForm"
 
 
 
-
 export default function App() {
+
+  const [totalDistance, setTotalDistance] = useState("");
+  const addDistance = (item) => {
+    setTotalDistance(totalDistance => [...totalDistance, item]);
+  }
+
+  console.log("Distance is", totalDistance)
+
+  if (totalDistance) {
+    totalDistance.map(item => tableData.push(item))
+  }
+
+
+
+  // useEffect(() => {
+
+  // }, [])
+
+  console.log("Table data is", tableData)
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
+        <br />
+        <br />
+        <img src="unnamed.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to re hjload.
         </p>
       </header>
-      <InputForm />
+      <InputForm distanceAdder={addDistance} />
     </div>
   );
 }
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,45 +1,72 @@
 import React, { useState, useEffect } from "react";
 import Moment from 'react-moment';
 import axios from "axios"
+import { Bar } from 'react-chartjs-2';
 
 
 
-export default function InputForm() {
+export default function InputForm(props) {
     const [timeAndDate, setTimeAndDate] = useState("");
     const [distance, setDistance] = useState("");
     const [timeTaken, setTimeTaken] = useState("");
     const [nameOfRun, setNameOfRun] = useState("");
     const [fetchedData, setFetchedData] = useState([]);
 
+    const disArray = [];
+    const label = [];
+    const chartDistance = [];
 
-    const submitRun = () => {
+
+    const submitRun = (props) => {
         const inputDetails = {
             "Time and Date of Run": timeAndDate,
             "Distance": distance,
             "Time taken": timeTaken,
             "Name of run": nameOfRun
         }
-
-
-
-
         console.log("Submitted details", inputDetails)
     }
 
+
+
     useEffect(() => {
+
         async function getData() {
             const result = await axios("api/runlog")
-            // .then((res) => {
-            // console.log(res.data)
-            // setFetchedData(res.data.json)
-            // console.log("Fetched Data", fetchedData)
-            // });
             console.log(result)
+            disArray.push(result)
             setFetchedData(result.data)
+
+            // setDistance(result.data[0].distance)
         }
         getData()
-
     }, []);
+
+    for (const obj of fetchedData) {
+        label.push(obj.id)
+        chartDistance.push(obj.distance)
+    }
+
+    const data = {
+        labels: label,
+        datasets: [
+            {
+                label: 'Distances in km',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: chartDistance
+            }
+        ]
+    };
+
+
+
+
+
+
 
     console.log("Fetched", fetchedData)
 
@@ -62,6 +89,17 @@ export default function InputForm() {
             <input type="text" placeholder="Time taken" onChange={e => setTimeTaken(e.target.value)} />
             <input type="text" placeholder="Name of run" onChange={e => setNameOfRun(e.target.value)} />
             <button onClick={submitRun}>Submit</button>
+            <div>
+                <h2>Bar Example (custom size)</h2>
+                <Bar
+                    data={data}
+                    width={150}
+                    height={50}
+                    options={{
+                        maintainAspectRatio: true
+                    }}
+                />
+            </div>
             <ul>
                 {fetchedData.map(item => (
                     <li key={item.id}>
@@ -73,6 +111,10 @@ export default function InputForm() {
                     </li>
                 ))}
             </ul>
+            <button onClick={(e) => {
+                props.distanceAdder({ disArray })
+            }}> Calculate Average Run</button>
+
         </div>
     )
 };
